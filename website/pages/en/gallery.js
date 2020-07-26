6350471c29834eb1b5a18c3d14acbf800f57aceb
Andrew Bernas <bandofpv@bandofpv.bandofpv>
2020-07-25 20:53:52 -0700

    Added photo gallery

--- website/pages/en/gallery.js
+++ website/pages/en/gallery.js
@@ -21,13 +21,74 @@ function About(props) {
   return (
     <div className="docMainWrapper wrapper">
       <div className="post">
-          <h1>About Me</h1>
+
+          <h1>Photography Gallery</h1>
+
           <MarkdownBlock>
-            [![airframe](img/about-me.png)](img/about-me.png)
 
-            [![airframe](img/rc-airplane.png)](img/rc-airplane.png)
+            [![Silver_Surfer](img/gallery/Silver_Surfer_AB.jpg)](img/gallery/large/Silver_Surfer_AB.jpg)
 
-            [![airframe](img/favicon.ico)](favicon.ico)
+            [![Rocky_Road](img/gallery/Rocky_Road_AB.jpg)](img/gallery/large/Rocky_Road_AB.jpg)
+
+            [![Path_not_Taken](img/gallery/Path_not_Taken.jpg)](img/gallery/large/Path_not_Taken.jpg)
+
+            [![Mirror_Lake_AB](img/gallery/Mirror_Lake_AB.jpg)](img/gallery/large/Mirror_Lake_AB.jpg)
+
+            [![Blue_AB](img/gallery/Blue_AB.jpg)](img/gallery/large/Blue_AB.jpg)
+
+            [![Confetti_AB](img/gallery/Confetti_AB.jpg)](img/gallery/large/Confetti_AB.jpg)
+
+            [![Swoosh](img/gallery/Swoosh.jpg)](img/gallery/large/Swoosh.jpg)
+
+            [![Cloud_of_Fish](img/gallery/Cloud_of_Fish.jpg)](img/gallery/large/Cloud_of_Fish.jpg)
+
+            [![Tropical_Waters](img/gallery/Tropical_Waters.jpg)](img/gallery/large/Tropical_Waters.jpg)
+
+            [![Pacific_Peace_AB](img/gallery/Pacific_Peace_AB.jpg)](img/gallery/large/Pacific_Peace_AB.jpg)
+
+            [![Walk_of_Gratitude_AB](img/gallery/Walk_of_Gratitude_AB.jpg)](img/gallery/large/Walk_of_Gratitude_AB.jpg)
+
+            [![Capture_the_Blast_AB](img/gallery/Capture_the_Blast_AB.jpg)](img/gallery/large/Capture_the_Blast_AB.jpg)
+
+            [![Cruisin_AB](img/gallery/Cruisin_AB.jpg)](img/gallery/large/Cruisin_AB.jpg)
+
+            [![Tunnel_Vision_AB](img/gallery/Tunnel_Vision_AB.jpg)](img/gallery/large/Tunnel_Vision_AB.jpg)
+
+            [![Night_Light_AB](img/gallery/Night_Light_AB.jpg)](img/gallery/large/Night_Light_AB.jpg)
+
+            [![1](img/gallery/1.jpg)](img/gallery/large/1.jpg)
+
+            [![2](img/gallery/2.jpg)](img/gallery/large/2.jpg)
+
+            [![3](img/gallery/3.jpg)](img/gallery/large/3.jpg)
+
+            [![4](img/gallery/4.jpg)](img/gallery/large/4.jpg)
+
+            [![Image15](img/gallery/Image15.jpg)](img/gallery/large/Image15.jpg)
+
+            [![Image23](img/gallery/Image23.jpg)](img/gallery/large/Image23.jpg)
+
+            [![Image3](img/gallery/Image3.jpg)](img/gallery/large/Image3.jpg)
+
+            [![Line_of_Sight_AB](img/gallery/Line_of_Sight_AB.jpg)](img/gallery/large/Line_of_Sight_AB.jpg)
+
+            [![Image9](img/gallery/Image9.jpg)](img/gallery/large/Image9.jpg)
+
+            [![Image11](img/gallery/Image11.jpg)](img/gallery/large/Image11.jpg)
+
+            [![Image18](img/gallery/Image18.jpg)](img/gallery/large/Image18.jpg)
+
+            [![fire](img/gallery/fire.jpg)](img/gallery/large/fire.jpg)
+
+            [![night](img/gallery/night.jpg)](img/gallery/large/night.jpg)
+
+            [![sunset](img/gallery/sunset.jpg)](img/gallery/large/sunset.jpg)
+
+            [![Circle_of_Life_AB](img/gallery/Circle_of_Life_AB.jpg)](img/gallery/large/Circle_of_Life_AB.jpg)
+
+            [![Jet_Bound_AB](img/gallery/Jet_Bound_AB.jpg)](img/gallery/large/Jet_Bound_AB.jpg)
+
+            [![Jakarta_Market_AB](img/gallery/Jakarta_Market_AB.jpg)](img/gallery/large/Jakarta_Market_AB.jpg)
 
           </MarkdownBlock>
       </div>
