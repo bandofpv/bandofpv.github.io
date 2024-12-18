a5a4d1aeb6e1c2314c758fc4669117b93c4d900a
bandofpv <andobernas@gmail.com>
2024-12-17 20:38:02 -0500

    Modified a few projects and added quad-sim WIP

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,16 +3,51 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '5ff'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', 'a2b'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
+    exact: true
+  },
+  {
     path: '/docs',
-    component: ComponentCreator('/docs', 'e5d'),
+    component: ComponentCreator('/docs', '1c8'),
     routes: [
       {
         path: '/docs',
-        component: ComponentCreator('/docs', 'e9a'),
+        component: ComponentCreator('/docs', 'ff7'),
         routes: [
           {
             path: '/docs',
-            component: ComponentCreator('/docs', 'b72'),
+            component: ComponentCreator('/docs', 'c77'),
             routes: [
               {
                 path: '/docs/category/code',
@@ -239,6 +274,12 @@ export default [
               {
                 path: '/docs/projects/school/ping-pong',
                 component: ComponentCreator('/docs/projects/school/ping-pong', '07d'),
+                exact: true,
+                sidebar: "projectsSidebar"
+              },
+              {
+                path: '/docs/projects/school/quad-sim',
+                component: ComponentCreator('/docs/projects/school/quad-sim', '1d1'),
                 exact: true,
                 sidebar: "projectsSidebar"
               },
