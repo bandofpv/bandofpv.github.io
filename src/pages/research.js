10686ee45302d693c48c0960df033b47e01c50b6
Andrew Bernas <25797752+bandofpv@users.noreply.github.com>
2026-01-20 15:24:21 -0500

    Published new features: Research page, re-arranged home page, fixed footer for mobile, fixed google search profile

--- src/pages/research.js
+++ src/pages/research.js
@@ -2,18 +2,18 @@ import React from 'react';
 import Layout from '@theme/Layout';
 
 const papers = [
-  {
-    title: 'Adaptive Nullspace-Model Parameter Identification of Multirotor Aircraft: Theory and Experimental Evaluation',
-    authors: 'Allan Elsberry, Louis Whitcomb, Levi DeVries, Andrew Bernas',
-    venue: 'IEEE International Conference on Robotics and Automation (ICRA), 2026',
-    link: 'https://example.com/paper-link',
-    abstract: 'This paper reports a Nullspace-based Adaptive Identification (NSAID) algorithm for simultaneously estimating plant model parameters and actuator model parameters for a multirotor aircraft with a 6-degree of freedom (DOF) second- order nonlinear dynamical model. A stability analysis is reported showing stability of all signals, and convergence of the parameter estimates to the set of true parameters under a Persistence of Excitation (PE) condition. Simulation and the first-reported ex- perimental results corroborate this analysis. Identifying accurate model parameters is essential for simulation, control, and fault detection and isolation. However, multirotor parameters such as mass, moments of inertia, and propeller thrust and drag coefficients may be unknown a priori and often change based on mission configuration or as components degrade. Unlike most existing model parameter identification methods, the proposed NSAID algorithm only requires vehicle attitude, velocity, and rotor speeds and does not require vehicle inertial acceleration measurements, which are corrupted by gravity and significant noise due to high-frequency vibrations inherent to multirotor systems. NSAID can be implemented in real-time with modest onboard computational resources.'
-  },
+//  {
+//    title: 'Adaptive Nullspace-Model Parameter Identification of Multirotor Aircraft: Theory and Experimental Evaluation',
+//    authors: 'Allan Elsberry, Louis Whitcomb, Levi DeVries, Andrew Bernas',
+//    venue: 'IEEE International Conference on Robotics and Automation (ICRA), 2026',
+//    link: 'https://example.com/paper-link',
+//    abstract: 'This paper reports a Nullspace-based Adaptive Identification (NSAID) algorithm for simultaneously estimating plant model parameters and actuator model parameters for a multirotor aircraft with a 6-degree of freedom (DOF) second- order nonlinear dynamical model. A stability analysis is reported showing stability of all signals, and convergence of the parameter estimates to the set of true parameters under a Persistence of Excitation (PE) condition. Simulation and the first-reported ex- perimental results corroborate this analysis. Identifying accurate model parameters is essential for simulation, control, and fault detection and isolation. However, multirotor parameters such as mass, moments of inertia, and propeller thrust and drag coefficients may be unknown a priori and often change based on mission configuration or as components degrade. Unlike most existing model parameter identification methods, the proposed NSAID algorithm only requires vehicle attitude, velocity, and rotor speeds and does not require vehicle inertial acceleration measurements, which are corrupted by gravity and significant noise due to high-frequency vibrations inherent to multirotor systems. NSAID can be implemented in real-time with modest onboard computational resources.'
+//  },
   {
     title: 'Relative Pose Estimation of an Unmanned Aerial System to a Naval Vessel Using Gaussian Splatting',
     authors: 'Andrew Bernas, Nolan Jess, Levi DeVries, Armon Shariati, Donald H. Costello.',
     venue: 'AIAA SciTech Forum, 2026',
-    link: 'https://doi.org/10.2514/6/6.2026-1123',
+    link: 'https://arc.aiaa.org/doi/10.2514/6.2026-1123',
     abstract: 'Precise relative pose estimation between Unmanned Aerial Vehicles (UAVs) and naval vessels is critical for autonomous operations, particularly in GPS-degraded environments where jamming or spoofing risks catastrophic failure. Addressing the limitations of current vision-based methods that often rely on physical markers, active lighting, or labor-intensive CAD annotations, this work presents a novel maritime localization approach using 3D Gaussian Splatting (3DGS) to estimate 6-DoF camera pose directly from a learned scene representation.  Utilizing the 6DGS framework, the pipeline inverts the rendering process via Radiant Ellicell ray- casting and a learned attention mechanism to solve for pose in a one-shot, closed-form manner without requiring an initial guess. Experimental validation performed with a quadcopter and a 1/10th scale Yard Patrol (YP) craft demonstrates that the system achieves high computational efficiency with an average inference latency of 0.012s (86Hz) and robust sub-meter accuracy in lateral and vertical translation, offering a scalable, real-time alternative for dynamic maritime environments.'
   },
   //Add more papers here
